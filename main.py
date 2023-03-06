b6d056ca28328cf892a003847350996e518838b0
Sherry-Ma <shaorong.ma@duke.edu>
2023-03-06 03:45:35 +0000

    done

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ app = Flask(__name__)
 def example():
     # here to change headers
     response_text = 'Hello, World!'
-    response = make_response(response_text, 200)
-    response.headers['Content-Length'] = len(response_text.encode('utf-8'))
-    response.headers['Cache-Control'] = 'max-age=360000'
+    headers = {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'max-age=360000',
+    }
+    response = make_response(jsonify(headers), 200) # here to change status code
+    response.headers = headers # set headers
+
     return response
 
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
